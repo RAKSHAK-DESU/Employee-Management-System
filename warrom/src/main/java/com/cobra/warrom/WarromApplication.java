8ffbbb2d35fde5042c8ff50ce58fd0856ab8474b
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-06-22 20:22:33 +0530

    Replace all files with new version

---
+++ warrom/src/main/java/com/cobra/warrom/WarromApplication.java
@@ -0,0 +1,13 @@
+package com.cobra.warrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarromApplication.class, args);
+	}
+
+}
---
+++ warrom/src/test/java/com/cobra/warrom/WarromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobra.warrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
