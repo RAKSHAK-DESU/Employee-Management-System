45aa3fc5df09c257eae253a5f04447149d4b06ca
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-06-23 17:36:37 +0530

    added

--- warrom/src/main/java/com/cobra/warrom/Product.java
+++ warrom/src/main/java/com/cobra/warrom/Product.java
@@ -3,4 +3,20 @@ package com.cobra.warrom;
 public class Product {
     private int pid;
     private String pname;
+
+    public void setPid(int pid){
+        this.pid=pid;
+    }
+
+    public int getPid(){
+        return pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
 }
